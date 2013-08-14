de3ee81a01712eacf852f0e8c6bf9cc2d3c2be05
dvdekow <dvdekow@gmail.com>
2013-08-14 16:07:56 +0700

    permission --not finished

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   before_filter :find_project, :only => [:show, :edit, :update, :destroy]
   before_filter :authorize_admin!, :except => [:index, :show]
+  before_filter :authenticate_user!, :only => [:index, :show]
   def index
     @projects = Project.all
   end
@@ -48,9 +49,10 @@ class ProjectsController < ApplicationController
 
   private
     def find_project
-      @project = Project.find(params[:id])
-      rescue ActiveRecord::RecordNotFound
-      flash[:alert] = "The project you were looking" + " for could not be found."
-      redirect_to projects_path
+        @project = Project.find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+        flash[:alert] = "The project you were looking" + " for could not be found."
+        redirect_to projects_path
     end
 end
+
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,11 @@
 class Project < ActiveRecord::Base
   attr_accessible :name
-  validates :name, :presence => true
+  validates :name, :presence => true, :uniqueness => true
+  
+  scope :readable_by, lambda { |user| joins (:permissions).where(:permissions => {:action => "view", :user_id => user.id})}
   has_many :tickets, :dependent => :destroy
+
+  def self.for(user)
+    user.admin? ? Project : Project.readable_by(user)
+  end
 end
